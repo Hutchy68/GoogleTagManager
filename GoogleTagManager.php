b1ef17a05a0fc0c5a913bb12b374e3f357d53225
Hutchy68 <Hutchy68@gmail.com>
2020-12-22 09:19:09 -0500

    Improvements and fixes

--- GoogleTagManager.php
+++ GoogleTagManager.php
@@ -2,16 +2,14 @@
 
 // Set the default global variables
 
-$wgAvailableRights[] = 'noanalytics';
-$wgGroupPermissions['sysop']['noanalytics'] = true;
-$wgGroupPermissions['bot']['noanalytics'] = true;
 $wgGoogleTagManagerContainerID = '';
-$wgGoogleTabManagerTagData = '';
+$wgGoogleTagManagerData = '';
 
 class TagManager {
 
 	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
-		global $wgGoogleTagManagerContainerID, $wgGoogleTagManagerIgnoreSysops, $wgGoogleTagManagerIgnoreBots, $wgUser;
+		global $wgGoogleTagManagerContainerID, $wgGoogleTagManagerData, $wgUser;
+    	if ( $wgGoogleTagManagerData != '' ) {  $wgGoogleTagManagerData = ' '.$wgGoogleTagManagerData; }
 		if ( $wgUser->isAllowed( 'noanalytics' ) ) {
 			$text = "\n<!-- Google Tag Manager tracking is disabled for this user -->";
         	$out->prependHTML($text);
@@ -25,7 +23,7 @@ class TagManager {
 			$text = "<!-- Google Tag Manager (added by extension GoogleTagManager) -->
 <noscript><iframe src=\"//www.googletagmanager.com/ns.html?id=$wgGoogleTagManagerContainerID\"
 height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<script type=\"text/plain\"$wgGoogleTagManagerData>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
