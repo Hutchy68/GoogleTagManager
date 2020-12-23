387e2e7a4891a635dddda179a37f864c69ea7bfa
Hutchy68 <Hutchy68@gmail.com>
2020-12-22 21:47:36 -0500

    Revert back to SkinAfterBottomScripts

--- GoogleTagManager.php
+++ GoogleTagManager.php
@@ -7,17 +7,15 @@ $wgGoogleTagManagerData = '';
 
 class TagManager {
 
-	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
+	public static function onSkinAfterBottomScripts( $skin, &$text='' ) {
 		global $wgGoogleTagManagerContainerID, $wgGoogleTagManagerData, $wgUser;
     	if ( $wgGoogleTagManagerData != '' ) {  $wgGoogleTagManagerData = ' '.$wgGoogleTagManagerData; }
 		if ( $wgUser->isAllowed( 'noanalytics' ) ) {
 			$text = "\n<!-- Google Tag Manager tracking is disabled for this user -->";
-        	$out->prependHTML($text);
         	return true;
 		}
 		if ( $wgGoogleTagManagerContainerID == '' ) {
 			$text = "\n<!-- Set \$wgGoogleTagManagerContainerID to your container ID provided by Google Tag Manager in LocalSettings.php. -->";
-        	$out->prependHTML($text);
         	return true;
 		} else {
 			$text = "<!-- Google Tag Manager (added by extension GoogleTagManager) -->
@@ -26,10 +24,9 @@ height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></nos
 <script type=\"text/plain\"$wgGoogleTagManagerData>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','$wgGoogleTagManagerContainerID');</script>
 <!-- End Google Tag Manager -->";
-        	$out->prependHTML($text);
     		return true;
         }
 
